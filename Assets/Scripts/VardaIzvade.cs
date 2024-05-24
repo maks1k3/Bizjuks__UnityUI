0b0a2c014cd0c6b4b44d32f850ddfa15ee57d56b
Maksims <maksims.bizjuks@sk.lvt.lv>
2024-05-24 09:21:10 +0300

    Praktiski paveikts

--- Assets/Scripts/VardaIzvade.cs
+++ Assets/Scripts/VardaIzvade.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 
 public class VardaIzvade : MonoBehaviour
 {
-   
+
     public string teksts;
     public InputField inputField;
     public InputField inputField1;
@@ -14,17 +14,17 @@ public class VardaIzvade : MonoBehaviour
 
     public void getText()
     {
-        
-            Debug.Log("ok");
-            string vards = inputField.text;
-            int dzimsanasG = int.Parse(inputField1.text);
-            int vecums = 2024 - dzimsanasG;
-            teksts = $"{vards} ir {vecums} gadus vecs!";
 
-            Debug.Log("ok");
-            izvade.text = teksts;
-        
-        
+       
+        string vards = inputField.text;
+        int dzimsanasG = int.Parse(inputField1.text);
+        int vecums = 2024 - dzimsanasG;
+        teksts = $"{vards} ir {vecums} gadus vecs!";
+
+        Debug.Log("ok");
+        izvade.text = teksts;
+
+         
     }
 
 }
